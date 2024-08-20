e955afc83b43825e72600635d2047bacaec459a2
Nenad Đapić <ndjapic@users.noreply.github.com>
2024-08-20 12:01:34 +0200

    Update treap.pas

--- samples/treap.pas
+++ samples/treap.pas
@@ -2,19 +2,19 @@ program SortedTreap;
 
 type
     TSortedTreap<_T> = class
-        Left, Right: TSortedTreap;
+        Left, Right: TSortedTreap<_T>;
         Key: _T;
         Priority: Integer;
         Size: Integer;
-        class function Compare(lhs, rhs: _T): Integer; static; abstract;
-        class function GetSize(Other: TSortedTreap): Integer; static;
+        class function Compare(lhs, rhs: _T): Integer; virtual;
+        class function GetSize(Other: TSortedTreap<_T>): Integer; static;
         procedure Update;
         constructor Create(K: _T);
         destructor Destroy; override;
-        procedure Split(K: _T; var L, R: TSortedTreap);
-        procedure Merge(L, R: TSortedTreap);
+        procedure Split(K: _T; var L, R: TSortedTreap<_T>);
+        procedure Merge(L, R: TSortedTreap<_T>);
         function Rank(K: _T): Integer;
-        procedure SplitByRank(i: Integer; var L, R: TSortedTreap); (* Needs implementation *)
+        procedure SplitByRank(i: Integer; var L, R: TSortedTreap<_T>); (* Needs implementation *)
         procedure Insort(K: _T);
         procedure Delete(K: _T);
         function GetAt(i: Integer): _T;
@@ -22,12 +22,14 @@ type
     end;
     TIntTreap = TSortedTreap<Integer>;
 
-class function TIntTreap.Compare(lhs, rhs: Integer): Integer;
+(* BEGIN TSortedTreap *)
+
+class function TSortedTreap<_T>.Compare(lhs, rhs: _T): Integer;
 begin
     Result := lhs - rhs;
 end;
 
-class function TSortedTreap.GetSize(Other: TSortedTreap): Integer;
+class function TSortedTreap<_T>.GetSize(Other: TSortedTreap<_T>): Integer;
 begin
     if Other = nil then
         Result := 0
@@ -35,12 +37,12 @@ begin
         Result := Other.Size;
 end;
 
-procedure TSortedTreap.Update;
+procedure TSortedTreap<_T>.Update;
 begin
     Size := GetSize(Left) + 1 + GetSize(Right);
 end;
 
-constructor TSortedTreap.Create(K: _T);
+constructor TSortedTreap<_T>.Create(K: _T);
 begin
     Inherited Create;
     Key := K;
@@ -50,14 +52,14 @@ begin
     Right := nil;
 end;
 
-destructor TSortedTreap.Destroy;
+destructor TSortedTreap<_T>.Destroy;
 begin
     if Left <> nil then Left.Free;
     if Right <> nil then Right.Free;
     Self.Free;
 end;
 
-procedure TSortedTreap.Split(K: _T; var L, R: TSortedTreap);
+procedure TSortedTreap<_T>.Split(K: _T; var L, R: TSortedTreap<_T>);
 begin
     if Compare(K, Key) < 0 then begin
         if Left = nil then
@@ -75,7 +77,7 @@ begin
     Update;
 end;
 
-procedure TSortedTreap.Merge(L, R: TSortedTreap);
+procedure TSortedTreap<_T>.Merge(L, R: TSortedTreap<_T>);
 begin
     if L = nil then
         Self := R
@@ -88,29 +90,50 @@ begin
         R.Merge(L.Right, R);
         Self := L;
     end;
-    Update;
+    if Self <> nil then Update;
 end;
 
-function TSortedTreap.Rank(K: _T): Integer;
+function TSortedTreap<_T>.Rank(K: _T): Integer;
 var
-    L, R: TSortedTreap;
+    L, R: TSortedTreap<_T>;
 begin
     Split(K, L, R);
 
     if L = nil then
-        Result := 0;
+        Result := 0
     else begin
         Result := L.Size;
         Merge(L, R);
     end;
 end;
 
-procedure TSortedTreap.Insort(K: _T);
+procedure TSortedTreap<_T>.SplitByRank(i: Integer; var L, R: TSortedTreap<_T>);
 var
-    L, M, R: TSortedTreap;
+    j: Integer;
+begin
+    j := i - GetSize(Left) - 1;
+    if j < 0 then begin
+        if Left = nil then
+            L := nil
+        else
+            Left.SplitByRank(i, L, Left);
+        R := Self;
+    end else begin
+        if Right = nil then
+            R := nil
+        else
+            Right.SplitByRank(j, Right, R);
+        L := Self;
+    end;
+    Update;
+end;
+
+procedure TSortedTreap<_T>.Insort(K: _T);
+var
+    L, M, R: TSortedTreap<_T>;
 begin
     Split(K, L, R);
-    M := TSortedTreap.Create(K);
+    M := TSortedTreap<_T>.Create(K);
     if R = nil then
         L.Merge(L, M)
     else
@@ -118,15 +141,15 @@ begin
     Merge(L, R);
 end;
 
-procedure TSortedTreap.Delete(K: _T);
+procedure TSortedTreap<_T>.Delete(K: _T);
 var
-    L, M, R: TSortedTreap;
+    L, M, R: TSortedTreap<_T>;
 begin
     Split(K, L, R);
 
     if R <> nil then begin
         R.SplitByRank(1, M, R);
-        if M.Key = K then begin
+        if M.Key = K then
             M.Free
         else if R = nil then
             R := M
@@ -136,6 +159,21 @@ begin
 
     Merge(L, R);
 end;
+
+function TSortedTreap<_T>.GetAt(i: Integer): _T;
+var
+    j: Integer;
+begin
+    j := i - GetSize(Left) - 1;
+    if (j < -1) and (Left <> nil) then
+        Result := Left.GetAt(i)
+    else if (j > -1) and (Right <> nil) then
+        Result := Right.GetAt(j)
+    else
+        Result := Key;
+end;
+
+(* END TSortedTreap *)
 
 var
     n, i, K: Integer;
@@ -164,7 +202,13 @@ begin
 end.
 
 (*
-* Input
-* 23
-* 2 3 4 5 6 7 8 9 4 9 16 25 36 49 64 81 8 27 64 16 81 32 64
-* *)
+Input
+23
+2 3 4 5 6 7 8 9 4 9 16 25 36 49 64 81 8 27 64 16 81 32 64
+
+Exit Code	139
+Exec Time	544 ms
+Memory	1050860 KB
+
+"When a container exits with status code 139, it's because it received a SIGSEGV signal. The operating system terminated the container's process to guard against a memory integrity violation. It's important to investigate what's causing the segmentation errors if your containers are terminating with code 139."
+*)
