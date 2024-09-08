31a9130aa76ba9660fb864ca1aaa4fd1f651f00d
Nenad Đapić <ndjapic@users.noreply.github.com>
2024-09-08 21:04:07 +0200

    Update treap.pas

--- samples/treap.pas
+++ samples/treap.pas
@@ -16,8 +16,8 @@ type
         class function Merge(var L, R: TSortedTreap<_T>): TSortedTreap<_T>; static;
         function Rank(K: _T): Integer;
         procedure SplitByRank(i: Integer; var L, R: TSortedTreap<_T>);
-        function Insort(K: _T): TSortedTreap<_T>;
-        function Discard(K: _T): TSortedTreap<_T>;
+        class procedure Insort(var M: TSortedTreap<_T>; K: _T); static;
+        class procedure Discard(var M: TSortedTreap<_T>; K: _T); static;
         procedure dfs;
         function GetAt(i: Integer): _T;
         property At[i: Integer]: _T Read GetAt; default;
@@ -125,21 +125,25 @@ begin
     Update;
 end;
 
-function TSortedTreap<_T>.Insort(K: _T): TSortedTreap<_T>;
+class procedure TSortedTreap<_T>.Insort(var M: TSortedTreap<_T>; K: _T);
 var
     L, R: TSortedTreap<_T>;
 begin
-    Split(K, L, R);
-    Result := TSortedTreap<_T>.Create(K);
-    Result := Merge(L, Result);
-    Result := Merge(Result, R);
+    if M = nil then begin
+        M := TSortedTreap<_T>.Create(K);
+    end else begin
+        M.Split(K, L, R);
+        M := TSortedTreap<_T>.Create(K);
+        M := Merge(L, M);
+        M := Merge(M, R);
+    end;
 end;
 
-function TSortedTreap<_T>.Discard(K: _T): TSortedTreap<_T>;
+class procedure TSortedTreap<_T>.Discard(var M: TSortedTreap<_T>; K: _T);
 var
-    L, M, R: TSortedTreap<_T>;
+    L, R: TSortedTreap<_T>;
 begin
-    Split(K, L, R);
+    M.Split(K, L, R);
 
     if R <> nil then begin
         R.SplitByRank(1, M, R);
@@ -152,7 +156,7 @@ begin
             R := Merge(M, R);
     end;
 
-    Result := Merge(L, R);
+    M := Merge(L, R);
 end;
 
 function TSortedTreap<_T>.GetAt(i: Integer): _T;
@@ -190,21 +194,18 @@ begin
 
     for i := 0 to n-1 do begin
         Read(K);
-        if a = nil then
-            a := TIntTreap.Create(K)
-        else
-            a := a.Insort(K);
-        {if i < 2 then a.dfs; WriteLn;}
+        TIntTreap.Insort(a, K);
+        Write('After Insort ', K, ': '); a.dfs; WriteLn;
     end;
     ReadLn;
 
-    a := a.Discard(9); // Delete one of two nines.
-    a := a.Discard(10); // Does nothing.
+    TIntTreap.Discard(a, 9); // Delete one of two nines.
+    TIntTreap.Discard(a, 10); // Does nothing.
     n := a.Size; // Update n.
 
-    for i := 0 to n-2 do begin
-      a := a.Discard(a[0]);
-      {Write('After Discard ', a[0], ': '); a.dfs; WriteLn;}
+    for i := 0 to n-1 do begin
+      Write('Before Discard ', a[0], ': '); a.dfs; WriteLn;
+      TIntTreap.Discard(a, a[0]);
     end;
     a.Free;
 end.
@@ -217,53 +218,53 @@ Standard Input
 
 Exit Code	0
 Exec Time	0 ms
-Memory	1628 KB
+Memory	1504 KB
 
 Standard Output
 
+After Insort 2: [2]
 After Insort 3: [[2]3]
 After Insort 4: [[[2]3]4]
 After Insort 5: [[[2]3]4[5]]
 After Insort 6: [[[2]3]4[5[6]]]
 After Insort 7: [[[2]3]4[5[[6]7]]]
-After Insort 8: [[[2]3]4[5[[6]7[8]]]]
-After Insort 9: [[[2]3]4[5[[6]7[[8]9]]]]
-After Insort 4: [[[2]3[4]]4[5[[6]7[[8]9]]]]
-After Insort 9: [[[[2]3[4]]4[5[[6]7[8]]]]9[9]]
-After Insort 16: [[[[2]3[4]]4[5[[6]7[8]]]]9[[9]16]]
-After Insort 25: [[[[2]3[4]]4[5[[6]7[8]]]]9[[9]16[25]]]
-After Insort 36: [[[[2]3[4]]4[5[[6]7[8]]]]9[[[9]16[25]]36]]
-After Insort 49: [[[[2]3[4]]4[5[[6]7[8]]]]9[[[9]16[25]]36[49]]]
-After Insort 64: [[[[2]3[4]]4[5[[6]7[8]]]]9[[[9]16[25]]36[49[64]]]]
-After Insort 81: [[[[2]3[4]]4[5[[6]7[8]]]]9[[[9]16[25]]36[49[[64]81]]]]
-After Insort 8: [[[[2]3[4]]4[5[[6]7[[8]8]]]]9[[[9]16[25]]36[49[[64]81]]]]
-After Insort 27: [[[[2]3[4]]4[5[[6]7[[8]8]]]]9[[[9]16[25]]27[36[49[[64]81]]]]]
-After Insort 64: [[[[2]3[4]]4[5[[6]7[[8]8]]]]9[[[9]16[25]]27[36[49[[[64]64]81]]]]]
-After Insort 16: [[[[2]3[4]]4[5[[6]7[[8]8]]]]9[[[9[16]]16[25]]27[36[49[[[64]64]81]]]]]
-After Insort 81: [[[[2]3[4]]4[5[[6]7[[8]8]]]]9[[[9[16]]16[25]]27[36[49[[[64]64]81[81]]]]]]
-After Insort 32: [[[[[2]3[4]]4[5[[6]7[[8]8]]]]9[[[9[16]]16[25]]27]]32[36[49[[[64]64]81[81]]]]]
-After Insort 64: [[[[[2]3[4]]4[5[[6]7[[8]8]]]]9[[[9[16]]16[25]]27]]32[[36[49]]64[[[64]64]81[81]]]]
-After Discard 9: [[[[[2]3[4]]4[5[[[6]7[[[8]8]9[16]]]16[25]]]]27]32[[36[49]]64[[[64]64]81[81]]]]
-After Discard 10: [[[[[2]3[4]]4[5[[[6]7[[[8]8]9[16]]]16[25]]]]27]32[[36[49]]64[[[64]64]81[81]]]]
-After Discard 2: [[[[3[4]]4[5[[[6]7[[[8]8]9[16]]]16[25]]]]27]32[[36[49]]64[[[64]64]81[81]]]]
-After Discard 3: [[[[4]4[5[[[6]7[[[8]8]9[16]]]16[25]]]]27]32[[36[49]]64[[[64]64]81[81]]]]
-After Discard 4: [[[4[5[[[6]7[[[8]8]9[16]]]16[25]]]]27]32[[36[49]]64[[[64]64]81[81]]]]
-After Discard 4: [[[5[[[6]7[[[8]8]9[16]]]16[25]]]27]32[[36[49]]64[[[64]64]81[81]]]]
-After Discard 5: [[[[[6]7[[[8]8]9[16]]]16[25]]27]32[[36[49]]64[[[64]64]81[81]]]]
-After Discard 6: [[[[7[[[8]8]9[16]]]16[25]]27]32[[36[49]]64[[[64]64]81[81]]]]
-After Discard 7: [[[[[[8]8]9[16]]16[25]]27]32[[36[49]]64[[[64]64]81[81]]]]
-After Discard 8: [[[[[8]9[16]]16[25]]27]32[[36[49]]64[[[64]64]81[81]]]]
-After Discard 8: [[[[9[16]]16[25]]27]32[[36[49]]64[[[64]64]81[81]]]]
-After Discard 9: [[[[16]16[25]]27]32[[36[49]]64[[[64]64]81[81]]]]
-After Discard 16: [[[16[25]]27]32[[36[49]]64[[[64]64]81[81]]]]
-After Discard 16: [[[25]27]32[[36[49]]64[[[64]64]81[81]]]]
-After Discard 25: [[27]32[[36[49]]64[[[64]64]81[81]]]]
-After Discard 27: [32[[36[49]]64[[[64]64]81[81]]]]
-After Discard 32: [[36[49]]64[[[64]64]81[81]]]
-After Discard 36: [[49]64[[[64]64]81[81]]]
-After Discard 49: [64[[[64]64]81[81]]]
-After Discard 64: [[[64]64]81[81]]
-After Discard 64: [[64]81[81]]
-After Discard 64: [81[81]]
-After Discard 81: [81]
+After Insort 8: [[[[2]3]4[5[[6]7]]]8]
+After Insort 9: [[[[2]3]4[5[[6]7]]]8[9]]
+After Insort 4: [[[[2]3[4]]4[5[[6]7]]]8[9]]
+After Insort 9: [[[[2]3[4]]4[5[[6]7]]]8[9[9]]]
+After Insort 16: [[[[2]3[4]]4[5[[6]7]]]8[9[[9]16]]]
+After Insort 25: [[[[2]3[4]]4[5[[6]7]]]8[9[[[9]16]25]]]
+After Insort 36: [[[[2]3[4]]4[5[[6]7]]]8[9[[[[9]16]25]36]]]
+After Insort 49: [[[[2]3[4]]4[5[[6]7]]]8[[9[[[[9]16]25]36]]49]]
+After Insort 64: [[[[2]3[4]]4[5[[6]7]]]8[[9[[[[9]16]25]36]]49[64]]]
+After Insort 81: [[[[2]3[4]]4[5[[6]7]]]8[[9[[[[9]16]25]36]]49[[64]81]]]
+After Insort 8: [[[[2]3[4]]4[5[[6]7]]]8[8[[9[[[[9]16]25]36]]49[[64]81]]]]
+After Insort 27: [[[[2]3[4]]4[5[[6]7]]]8[8[[9[[[[9]16]25[27]]36]]49[[64]81]]]]
+After Insort 64: [[[[2]3[4]]4[5[[6]7]]]8[8[[[9[[[[9]16]25[27]]36]]49]64[[64]81]]]]
+After Insort 16: [[[[2]3[4]]4[5[[6]7]]]8[[8[9[9]]]16[[[[[16]25[27]]36]49]64[[64]81]]]]
+After Insort 81: [[[[[2]3[4]]4[5[[6]7]]]8[[8[9[9]]]16[[[[[16]25[27]]36]49]64[64]]]]81[81]]
+After Insort 32: [[[[[2]3[4]]4[5[[6]7]]]8[[8[9[9]]]16[[[[[[16]25[27]]32]36]49]64[64]]]]81[81]]
+After Insort 64: [[[[[2]3[4]]4[5[[6]7]]]8[[8[9[9]]]16[[[[[[16]25[27]]32]36]49]64[64[64]]]]]81[81]]
+Before Discard 2: [[[[[2]3[4]]4[5[[6]7]]]8[[8[9]]16[[[[[[16]25[27]]32]36]49]64[64[64]]]]]81[81]]
+Before Discard 3: [[[[3[4]]4[5[[6]7]]]8[[8[9]]16[[[[[[16]25[27]]32]36]49]64[64[64]]]]]81[81]]
+Before Discard 4: [[[[4]4[5[[6]7]]]8[[8[9]]16[[[[[[16]25[27]]32]36]49]64[64[64]]]]]81[81]]
+Before Discard 4: [[[4[5[[6]7]]]8[[8[9]]16[[[[[[16]25[27]]32]36]49]64[64[64]]]]]81[81]]
+Before Discard 5: [[[5[[6]7]]8[[8[9]]16[[[[[[16]25[27]]32]36]49]64[64[64]]]]]81[81]]
+Before Discard 6: [[[[6]7]8[[8[9]]16[[[[[[16]25[27]]32]36]49]64[64[64]]]]]81[81]]
+Before Discard 7: [[[7]8[[8[9]]16[[[[[[16]25[27]]32]36]49]64[64[64]]]]]81[81]]
+Before Discard 8: [[8[[8[9]]16[[[[[[16]25[27]]32]36]49]64[64[64]]]]]81[81]]
+Before Discard 8: [[[8[9]]16[[[[[[16]25[27]]32]36]49]64[64[64]]]]81[81]]
+Before Discard 9: [[[9]16[[[[[[16]25[27]]32]36]49]64[64[64]]]]81[81]]
+Before Discard 16: [[16[[[[[[16]25[27]]32]36]49]64[64[64]]]]81[81]]
+Before Discard 16: [[[[[[[16]25[27]]32]36]49]64[64[64]]]81[81]]
+Before Discard 25: [[[[[[25[27]]32]36]49]64[64[64]]]81[81]]
+Before Discard 27: [[[[[[27]32]36]49]64[64[64]]]81[81]]
+Before Discard 32: [[[[[32]36]49]64[64[64]]]81[81]]
+Before Discard 36: [[[[36]49]64[64[64]]]81[81]]
+Before Discard 49: [[[49]64[64[64]]]81[81]]
+Before Discard 64: [[64[64[64]]]81[81]]
+Before Discard 64: [[64[64]]81[81]]
+Before Discard 64: [[64]81[81]]
+Before Discard 81: [81[81]]
+Before Discard 81: [81]
 *)
