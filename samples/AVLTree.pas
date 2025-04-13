c392149bc2da65525f135f54210c9d5b4f90eaad
Nenad Đapić <ndjapic@users.noreply.github.com>
2025-04-13 22:50:32 +0200

    Update AVLTree.pas

--- samples/AVLTree.pas
+++ samples/AVLTree.pas
@@ -8,27 +8,35 @@ const
 type
     TAVLTree<_T> = class
         x: _T;
-        c: int32;
+        elementCount, treeSize: int32;
         h: int8;
         l, r: TAVLTree<_T>;
+        class function Compare(lhs, rhs: _T): SizeInt; inline;
         constructor Create(x: _T);
         destructor Destroy; override;
-        class function GetCount(t: TAVLTree<_T>): int32; inline;
+        class function GetTreeSize(t: TAVLTree<_T>): int32; inline;
         class function GetHeight(t: TAVLTree<_T>): int8;
         function GetBalance(): int8; inline;
+        class function GetElementCount(t: TAVLTree<_T>; x: _T): int32;
         procedure UpdateNode(); inline;
         class procedure RotateL(var t: TAVLTree<_T>); inline;
         class procedure RotateR(var t: TAVLTree<_T>); inline;
         class procedure Add(var t: TAVLTree<_T>; x: _T);
-        {class procedure Discard(var t: TAVLTree<_T>; x: _T);
+        class procedure Discard(var t: TAVLTree<_T>; x: _T);
         class procedure GetAt(t: TAVLTree<_T>; i: int32; var x: _T);
-        class function GetRank(t: TAVLTree<_T>; x: _T): int32;}
+        class function GetRank(t: TAVLTree<_T>; x: _T): int32;
     end;
+
+class function TAVLTree<_T>.Compare(lhs, rhs: _T): SizeInt;
+begin
+    Result := lhs - rhs;
+end;
 
 constructor TAVLTree<_T>.Create(x: _T);
 begin
     Self.x := x;
-    c := 1;
+    elementCount := 1;
+    treeSize := 1;
     h := 1;
     l := nil;
     r := nil;
@@ -41,12 +49,12 @@ begin
     inherited;
 end;
 
-class function TAVLTree<_T>.GetCount(t: TAVLTree<_T>): int32;
+class function TAVLTree<_T>.GetTreeSize(t: TAVLTree<_T>): int32;
 begin
     if t = nil then
         Result := 0
     else
-        Result := t.c;
+        Result := t.treeSize;
 end;
 
 class function TAVLTree<_T>.GetHeight(t: TAVLTree<_T>): int8;
@@ -54,7 +62,7 @@ begin
     if t = nil then
         Result := 0
     else
-        Result := t.c;
+        Result := t.h;
 end;
 
 function TAVLTree<_T>.GetBalance(): int8;
@@ -65,9 +73,21 @@ begin
         Result := GetHeight(r) - GetHeight(l);
 end;
 
+class function TAVLTree<_T>.GetElementCount(t: TAVLTree<_T>; x: _T): int32;
+begin
+    if t = nil then
+        Result := 0
+    else if Compare(x, t.x) < 0 then
+        Result := GetElementCount(t.l, x)
+    else if Compare(x, t.x) > 0 then
+        Result := GetElementCount(t.r, x)
+    else
+        Result := t.elementCount;
+end;
+
 procedure TAVLTree<_T>.UpdateNode();
 begin
-    c := GetCount(l) + GetCount(r) + 1;
+    treeSize := GetTreeSize(l) + GetTreeSize(r) + elementCount;
     h := max(GetHeight(l), GetHeight(r)) + 1;
 end;
 
@@ -79,7 +99,7 @@ begin
     t.r := r.l;
     r.l := t;
     t.UpdateNode();
-    r.UpdateNode();
+    {r.UpdateNode(); Is this neccessary? There will be update after this.}
     t := r;
 end;
 
@@ -91,65 +111,145 @@ begin
     t.l := l.r;
     l.r := t;
     t.UpdateNode();
-    l.UpdateNode();
+    {l.UpdateNode(); Is this neccessary? There will be update after this.}
     t := l;
 end;
 
 class procedure TAVLTree<_T>.Add(var t: TAVLTree<_T>; x: _T);
 begin
     if t = nil then
-        t := Create(x)
-    else if x < t.x then begin
+        t := TAVLTree<int32>.Create(x)
+    else if Compare(x, t.x) < 0 then begin
+
         Add(t.l, x);
         if t.GetBalance() < -1 then begin
             if t.l.GetBalance() > 0 then RotateL(t.l);
             RotateR(t);
         end;
-    end else begin
+
+    end else if Compare(x, t.x) > 0 then begin
+
         Add(t.r, x);
-        if t.GetBalance() > -1 then begin
+        if t.GetBalance() > 1 then begin
             if t.r.GetBalance() < 0 then RotateR(t.r);
             RotateL(t);
+        end;
+
+    end else
+        inc(t.elementCount);
+
+    t.UpdateNode();
+end;
+
+class procedure TAVLTree<_T>.Discard(var t: TAVLTree<_T>; x: _T);
+var
+    temp: TAVLTree<_T>;
+begin
+    if t <> nil then begin
+        if Compare(x, t.x) < 0 then
+            Discard(t.l, x)
+        else if Compare(x, t.x) > 0 then
+            Discard(t.r, x)
+        else begin // Element found
+            if t.elementCount > 1 then begin
+                dec(t.elementCount);
+            end else begin
+                if t.l = nil then begin
+                    temp := t;
+                    t := t.r;
+                    temp.l := nil; // Set children to nil before freeing
+                    temp.r := nil;
+                    temp.Free;
+                end else if t.r = nil then begin
+                    temp := t;
+                    t := t.l;
+                    temp.l := nil; // Set children to nil before freeing
+                    temp.r := nil;
+                    temp.Free;
+                end else begin // Node with two children
+                    temp := t.r;
+                    while temp.l <> nil do
+                        temp := temp.l;
+                    t.x := temp.x;
+                    t.elementCount := temp.elementCount;
+                    temp.elementCount := 1;
+                    Discard(t.r, temp.x);
+                end;
+            end;
+        end;
+
+        if t <> nil then begin
+            t.UpdateNode();
+            if t.GetBalance() < -1 then begin
+                if (t.l <> nil) and (t.l.GetBalance() > 0) then RotateL(t.l);
+                RotateR(t);
+                t.UpdateNode();
+            end else if t.GetBalance() > 1 then begin
+                if (t.r <> nil) and (t.r.GetBalance() < 0) then RotateR(t.r);
+                RotateL(t);
+                t.UpdateNode();
+            end;
+        end;
+    end;
+end;
+
+class procedure TAVLTree<_T>.GetAt(t: TAVLTree<_T>; i: int32; var x: _T);
+var
+    leftSize: int32;
+begin
+    if t <> nil then begin
+        leftSize := GetTreeSize(t.l);
+        if i < leftSize then
+            GetAt(t.l, i, x)
+        else if i < leftSize + t.elementCount then
+            x := t.x
+        else
+            GetAt(t.r, i - leftSize - t.elementCount, x);
+    end;
+end;
+
+class function TAVLTree<_T>.GetRank(t: TAVLTree<_T>; x: _T): int32;
+begin
+    Result := 0;
+    while t <> nil do begin
+        if Compare(x, t.x) < 0 then
+            t := t.l
+        else if Compare(x, t.x) > 0 then begin
+            Result := Result + GetTreeSize(t.l) + t.elementCount;
+            t := t.r;
+        end else begin
+            Result := Result + GetTreeSize(t.l);
+            Exit;
         end;
     end;
 end;
 
 var
-    i: int32;
+    n, i: int32;
     t: TAVLTree<int32>;
 
 begin
     randomize;
     t := nil;
+    n := nn div 10;
 
-    for i := 1 to nn do TAVLTree<int32>.Add(t, random(nn));
-    writeln(
-        ' c=', TAVLTree<int32>.GetCount(t),
-        ' h=', TAVLTree<int32>.GetHeight(t)
-    );
+    for i := 1 to nn do begin
 
-    if t <> nil then t.Free;
+        if odd(i div n) then
+            TAVLTree<int32>.Add(t, random(nn))
+        else
+            TAVLTree<int32>.Discard(t, random(nn));
 
-    {try
-        readln(n);
-        for i := 0 to n-1 do begin
-            read(x);
-            TAVLTree<int32>.Add(t, x);
-        end;
-        readln;
+        if i mod n = 0 then
+            writeln(
+                ' size=', TAVLTree<int32>.GetTreeSize(t),
+                ' height=', TAVLTree<int32>.GetHeight(t),
+                ' element=', t.x,
+                ' elementCount=', t.elementCount
+            );
 
-        for i := 0 to TAVLTree<int32>.GetCount(t) - 1 do begin
-            TAVLTree<int32>.GetAt(t, i, x);
-            write(' ', x);
-        end;
-        writeln;
-    finally
+    end;
+
+    if t <> nil then
         t.Free;
-    end;}
 end.
-
-(*
-Улазни подаци:
-14
-2 4 8 16 32 64 3 9 27 81 5 25 7 49
-*)
