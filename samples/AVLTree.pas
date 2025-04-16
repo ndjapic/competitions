353793606404924a55b4eef7697caf94695fb605
Nenad Đapić <ndjapic@users.noreply.github.com>
2025-04-16 22:43:03 +0200

    Update AVLTree.pas

--- samples/AVLTree.pas
+++ samples/AVLTree.pas
@@ -150,32 +150,26 @@ begin
             Discard(t.l, x)
         else if Compare(x, t.x) > 0 then
             Discard(t.r, x)
-        else begin // Element found
-            if t.elementCount > 1 then begin
-                dec(t.elementCount);
-            end else begin
-                if t.l = nil then begin
-                    temp := t;
-                    t := t.r;
-                    temp.l := nil; // Set children to nil before freeing
-                    temp.r := nil;
-                    temp.Free;
-                end else if t.r = nil then begin
-                    temp := t;
-                    t := t.l;
-                    temp.l := nil; // Set children to nil before freeing
-                    temp.r := nil;
-                    temp.Free;
-                end else begin // Node with two children
-                    temp := t.r;
-                    while temp.l <> nil do
-                        temp := temp.l;
-                    t.x := temp.x;
-                    t.elementCount := temp.elementCount;
-                    temp.elementCount := 1;
-                    Discard(t.r, temp.x);
-                end;
-            end;
+        else if t.elementCount > 1 then begin // Element found
+            dec(t.elementCount);
+        end else if t.l = nil then begin
+            temp := t;
+            t := t.r;
+            temp.r := nil; // Set children to nil before freeing
+            temp.Free;
+        end else if t.r = nil then begin
+            temp := t;
+            t := t.l;
+            temp.l := nil; // Set children to nil before freeing
+            temp.Free;
+        end else begin // Node with two children
+            temp := t.r;
+            while temp.l <> nil do
+                temp := temp.l;
+            t.x := temp.x;
+            t.elementCount := temp.elementCount;
+            temp.elementCount := 1;
+            Discard(t.r, temp.x);
         end;
 
         if t <> nil then begin
@@ -183,11 +177,11 @@ begin
             if t.GetBalance() < -1 then begin
                 if (t.l <> nil) and (t.l.GetBalance() > 0) then RotateL(t.l);
                 RotateR(t);
-                t.UpdateNode();
+                t.UpdateNode(); // Re-update after rotation
             end else if t.GetBalance() > 1 then begin
                 if (t.r <> nil) and (t.r.GetBalance() < 0) then RotateR(t.r);
                 RotateL(t);
-                t.UpdateNode();
+                t.UpdateNode(); // Re-update after rotation
             end;
         end;
     end;
@@ -228,19 +222,35 @@ var
     n, i: int32;
     t: TAVLTree<int32>;
 
+procedure dfs(t: TAVLTree<int32>);
+begin
+    if t <> nil then begin
+        write('[');
+        dfs(t.l);
+        write(t.x);
+        write('*');
+        write(t.elementCount);
+        dfs(t.r);
+        write(']');
+    end;
+end;
+
 begin
     randomize;
     t := nil;
     n := nn div 10;
 
-    for i := 1 to nn do begin
+    for i := 0 to nn-1 do begin
 
         if odd(i div n) then
-            TAVLTree<int32>.Add(t, random(nn))
+            TAVLTree<int32>.Discard(t, random(nn))
         else
-            TAVLTree<int32>.Discard(t, random(nn));
+            TAVLTree<int32>.Add(t, random(nn));
 
-        if i mod n = 0 then
+        if (i+1) mod n > 0 then
+        else if t = nil then
+            writeln('nil')
+        else
             writeln(
                 ' size=', TAVLTree<int32>.GetTreeSize(t),
                 ' height=', TAVLTree<int32>.GetHeight(t),
@@ -252,4 +262,16 @@ begin
 
     if t <> nil then
         t.Free;
+end.
+
+begin
+    t := nil;
+    for i := 1 to 9 do TAVLTree<int32>.Add(t, i*i);
+    dfs(t); writeln;
+    for i := 4 downto 1 do TAVLTree<int32>.Add(t, i*i*i);
+    dfs(t); writeln;
+    for i := 1 to 3 do TAVLTree<int32>.Add(t, i*i*i*i);
+    dfs(t); writeln;
+    for i := 1 to 90 do TAVLTree<int32>.Discard(t, i);
+    dfs(t); writeln;
 end.
