89ca72a9b17600d482107fa49fd33ad11c6793c6
Nenad Đapić <ndjapic@users.noreply.github.com>
2025-08-24 11:41:52 +0200

    Update heapq.pas

--- samples/heapq.pas
+++ samples/heapq.pas
@@ -7,102 +7,111 @@ type
 		function Compare(constref Left, Right: _T): Integer;
 	end;
 	THeap<_T> = class
+	private
+		FItems: TList<_T>;
+		// Privatna funkcija za poređenje elemenata
+		function Favorite(u: SizeInt): SizeInt;
+		function GetItem(Index: SizeInt): _T;
+		function GetCount: SizeInt;
 	public
-		Items: TList<_T>;
 		constructor Create;
 		destructor Destroy; override;
 		procedure Push(Item: _T);
-		function Favorite(u: SizeInt): SizeInt;
 		function Pop: _T;
+		property Count: SizeInt read GetCount;
+		property Items[Index: SizeInt]: _T read GetItem; default;
 	end;
-var
-	q, x, i, tp: int8;
-	Comparer: TMyComparer<int8>;
-	pq: THeap<int8>;
 
-function TMyComparer<_T>.Compare(constref Left, Right: _T): Integer;
 begin
 	Result := Left - Right;
+end;
+
+// Implementacija metoda THeap<_T>
+function THeap<_T>.Favorite(u: SizeInt): SizeInt;
+var
+	v: SizeInt;
+begin
+	v := u * 2 + 2;
+	if (v >= FItems.Count) or (Compare(FItems[v-1], FItems[v]) < 0) then
+		dec(v);
+	Result := v;
+end;
+
+function THeap<_T>.GetItem(Index: SizeInt): _T;
+begin
+	Result := FItems[Index];
+end;
+
+function THeap<_T>.GetCount: SizeInt;
+begin
+	Result := FItems.Count;
 end;
 
 constructor THeap<_T>.Create;
 begin
 	Inherited Create;
-	Items := TList<_T>.Create;
+	FItems := TList<_T>.Create;
 end;
 
 destructor THeap<_T>.Destroy;
 begin
-	// Perform cleanup specific to THeap here
-	Items.Free;
-	inherited Destroy; // Call the parent destructor
+	FItems.Free;
+	inherited Destroy;
 end;
 
 procedure THeap<_T>.Push(Item: _T);
 var
 	u, v: SizeInt;
 begin
-	v := Items.Count;
-	Items.Add(Item);
+	v := FItems.Count;
+	FItems.Add(Item);
 	u := (v-1) div 2;
-	while (v > 0) and (Comparer.Compare(Items[v], Items[u]) < 0) do begin
-		Items.Exchange(u, v);
+	while (v > 0) and (Compare(FItems[v], FItems[u]) < 0) do begin
+		FItems.Exchange(u, v);
 		v := u;
 		u := (v-1) div 2;
 	end;
-end;
-
-function THeap<_T>.Favorite(u: SizeInt): SizeInt;
-var
-	v: SizeInt;
-begin
-	v := u * 2 + 2;
-	if (v >= Items.Count) or (Comparer.Compare(Items[v], Items[v-1]) >= 0) then
-		dec(v);
-	Result := v;
 end;
 
 function THeap<_T>.Pop: _T;
 var
 	u, v: SizeInt;
 begin
-	Result := Items[0]; // Get the first element
-	Items[0] := Items[Items.Count - 1];
 	// If the Item itself needs to be freed, free it before deleting from the list
-	// Items[Items.Count - 1].Free;
-	// FreeAndNil(Items[Items.Count - 1]);
-	Items.Delete(Items.Count - 1); // Remove the last element
+	Result := FItems[0];
+	FItems[0] := FItems[FItems.Count - 1];
+	FItems.Delete(FItems.Count - 1);
+
 	u := 0;
 	v := Favorite(u);
-	while (v < Items.Count) and (Comparer.Compare(Items[v], Items[u]) < 0) do begin
-		Items.Exchange(u, v);
+	while (v < FItems.Count) and (Compare(FItems[v], FItems[u]) < 0) do begin
+		FItems.Exchange(u, v);
 		u := v;
 		v := Favorite(u);
 	end;
 end;
 
+// Glavni programski blok
+var
+	q, x, i, tp: int8;
+	pq: THeap<int8>;
+
 begin
 	Comparer := TMyComparer<int8>.Create;
+	readln(q);
 	pq := THeap<int8>.Create;
 	try
-
-		readln(q);
-
 		for i := 1 to q do begin
 			read(tp);
 			case tp of
-
 				1: begin
 					read(x);
 					pq.Push(x);
 				end;
-
 				2: writeln(pq.Pop);
-
 			end;
 			readln;
 		end;
-
 	finally
 		pq.Free;
 		Comparer.Free;
