0b006c949bf8920728d48b8edc623574507b122d
Nenad Đapić <ndjapic@users.noreply.github.com>
2025-12-30 12:12:27 +0100

    Codeforces contest added, atcoder updated

--- atcoder/abc438/D_Tail_of_Snake.pas
+++ atcoder/abc438/D_Tail_of_Snake.pas
@@ -4,38 +4,23 @@ uses
 const
 	nn = 300 * 1000;
 var
-	n, i, x, y: int32;
-	ans: int64;
-	a, b, c: array [0 .. nn] of int64;
+	n, i, j: int32;
+	a: array [1 .. 3, 1 .. nn] of int64;
 
 begin
 	readln(n);
 
-	a[0] := 0;
-	for i := 1 to n do begin
-		read(a[i]);
-		inc(a[i], a[i-1]);
+	for i := 1 to 3 do begin
+		for j := 1 to n do read(a[i, j]); readln;
 	end;
-	readln;
 
-	b[0] := 0;
-	for i := 1 to n do begin
-		read(b[i]);
-		inc(b[i], b[i-1]);
+	for j := 2 to n do inc(a[1, j], a[1, j-1]);
+
+	for i := 2 to 3 do begin
+		inc(a[i, i], a[i-1, i-1]);
+		for j := i+1 to n do
+			inc(a[i, j], max(a[i-1, j-1], a[i, j-1]));
 	end;
-	readln;
 
-	c[0] := 0;
-	for i := 1 to n do begin
-		read(c[i]);
-		inc(c[i], c[i-1]);
-	end;
-	readln;
-
-	ans := 0;
-	for x := 1 to n-2 do
-		for y := x+1 to n-1 do
-			ans := max(ans, a[x] + b[y] - b[x] + c[n] - c[y]);
-
-	writeln(ans);
+	writeln(a[3, n]);
 end.
