60db8955c2d0f145a617123211aaf562c8f44504
Nenad Đapić <ndjapic@users.noreply.github.com>
2026-02-07 13:13:33 +0100

    Update D.py

--- petlja/os-opstinsko-202526-8n/D.py
+++ petlja/os-opstinsko-202526-8n/D.py
@@ -1,15 +1,8 @@
 from itertools import product
 
-n1 = int(input())
-s1 = input().split()
-n2 = int(input())
-s2 = input().split()
-n3 = int(input())
-s3 = input().split()
+n1, s1 = int(input()), input().split()
+n2, s2 = int(input()), input().split()
+n3, s3 = int(input()), input().split()
 
-nacini = 0
-for m1, m2, m3 in product(s1, s2, s3):
-	if len(set([m1, m2, m3])) == 3:
-		nacini += 1
-
-print(nacini)
+nacini = [v for v in product(s1, s2, s3) if len(set(v)) == 3]
+print(len(nacini))
