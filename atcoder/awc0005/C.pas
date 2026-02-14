c87a06f656be0dcd5f8b2384d4d31d8eb23be68c
Nenad Đapić <ndjapic@users.noreply.github.com>
2026-02-14 04:38:20 +0100

    Update C.pas

--- atcoder/awc0005/C.pas
+++ atcoder/awc0005/C.pas
@@ -7,8 +7,11 @@ var
 	n, k, i: int32;
 	ans: int64;
 	a, b: array [1 .. nn] of int64;
+	InputBuf, OutputBuf: array [1 .. 65536] of Char;
 
 begin
+	SetTextBuf(Input, InputBuf);
+	SetTextBuf(Output, OutputBuf);
 	readln(n, k);
 
 	for i := 1 to n do read(a[i]);
