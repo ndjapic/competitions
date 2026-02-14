aa45d02e24bb82071e068ff40b2e21fa0fc0a3a5
Nenad Đapić <ndjapic@users.noreply.github.com>
2026-02-14 04:37:58 +0100

    Update A.pas

--- atcoder/awc0005/A.pas
+++ atcoder/awc0005/A.pas
@@ -2,8 +2,11 @@ program _A;
 var
 	n, k, i, p: int32;
 	ans: int64;
+	InputBuf, OutputBuf: array [1 .. 65536] of Char;
 
 begin
+	SetTextBuf(Input, InputBuf);
+	SetTextBuf(Output, OutputBuf);
 	readln(n, k);
 
 	ans := 0;
