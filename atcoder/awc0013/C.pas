3b70279fed2b96a4d45bc031f9175bbea4fa5fe7
Nenad Đapić <ndjapic@users.noreply.github.com>
2026-02-25 22:49:05 +0100

    Update C.pas

--- atcoder/awc0013/C.pas
+++ atcoder/awc0013/C.pas
@@ -1,41 +1,22 @@
 program _C;
 {$MODE DELPHI}{$OPTIMIZATION LEVEL3,ON}
-uses
-	generics.collections, generics.defaults;
 const
-	mm = 10;
+	nn = 100 * 1000;
 var
-	n, i: int32;
-	m, k: int8;
-	aik, s, total: int64;
-	a: array [1 .. mm] of tlist<int64>;
+	n, k, i, c: int32;
+	s: array [1 .. nn] of int32;
 	InputBuf, OutputBuf: array [1 .. 65536] of Char;
 
 begin
 	SetTextBuf(Input, InputBuf);
 	SetTextBuf(Output, OutputBuf);
 
-	readln(n, m);
+	readln(n, k);
 
-	for k := 1 to m do a[k] := tlist<int64>.create;
+	for i := 1 to n do read(s[i]); readln;
 
-	for i := 1 to n do begin
-		read(aik);
-		a[k].add(aik);
-	end;
-	readln;
-
-	total := 0;
-	for k := 1 to m do begin
-		a[k].sort;
-		s := 0;
-		for i := 0 to n-1 do begin
-			inc(total, a[k][i] * i - s);
-			inc(s, a[k][i]);
-		end;
-	end;
-
-	writeln(total);
-
-	for k := 1 to m do a[k].free;
+	c := 0;
+	for i := 1 to n do
+		if s[i] < s[k] then inc(c);
+	writeln(c);
 end.
