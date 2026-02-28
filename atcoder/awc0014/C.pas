51c31f45e8ac1521414ef24171d7bf747109ed2e
Nenad Đapić <ndjapic@users.noreply.github.com>
2026-02-28 01:51:42 +0100

    Create C.pas

---
+++ atcoder/awc0014/C.pas
@@ -0,0 +1,30 @@
+program _C;
+{$MODE DELPHI}{$OPTIMIZATION LEVEL3,ON}
+uses
+	math;
+const
+	nn = 200 * 1000;
+var
+	g, d, k, v: int32;
+	m: int64;
+	one: extended;
+	ans: boolean;
+	InputBuf, OutputBuf: array [1 .. 65536] of Char;
+
+begin
+	SetTextBuf(Input, InputBuf);
+	SetTextBuf(Output, OutputBuf);
+
+	readln(g, m, d, k, v);
+	one := 1.0;
+
+	if int64(d) * k > g then
+		ans := one * g * d <= one * (m-g) / v
+	else
+		ans := one * (1-d) * k + g <= one * (m-g) / v;
+
+	if ans then
+		writeln('Yes')
+	else
+		writeln('No');
+end.
