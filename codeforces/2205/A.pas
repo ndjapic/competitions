195f0e02dc13e2963571dc97c5606bfa8f8a69fc
Nenad Đapić <ndjapic@users.noreply.github.com>
2026-02-28 01:55:47 +0100

    Create A.pas

---
+++ codeforces/2205/A.pas
@@ -0,0 +1,31 @@
+program _A;
+{$MODE DELPHI}{$OPTIMIZATION LEVEL3,ON}
+const
+	nn = 500;
+var
+	notc, tci, n, i, j: int32;
+	p: array [1 .. nn] of int32;
+	InputBuf, OutputBuf: array [1 .. 65536] of Char;
+
+begin
+	SetTextBuf(Input, InputBuf);
+	SetTextBuf(Output, OutputBuf);
+	readln(notc);
+	for tci := 1 to notc do begin
+
+		readln(n);
+
+		for i := 1 to n do begin
+			read(p[i]);
+			if p[i] = n then j := i;
+		end;
+		readln;
+
+		p[j] := p[1];
+		p[1] := n;
+
+		for i := 1 to n-1 do write(p[i], ' ');
+		writeln(p[n]);
+
+	end;
+end.
