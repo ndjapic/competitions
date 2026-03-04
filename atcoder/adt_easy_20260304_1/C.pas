ba6b5cee46feead978dc450f3c14f5b204077a31
Nenad Đapić <ndjapic@users.noreply.github.com>
2026-03-04 08:27:55 +0100

    Update C.pas

--- atcoder/adt_easy_20260304_1/C.pas
+++ atcoder/adt_easy_20260304_1/C.pas
@@ -1,22 +1,24 @@
 program _C;
 {$MODE DELPHI}{$OPTIMIZATION LEVEL3,ON}
 const
-	nn = 200 * 1000;
+	nn = 100;
 var
-	a, b, culprit: int8;
+	n, i, l, r: int8;
+	lock: array [1 .. nn] of int8;
 	InputBuf, OutputBuf: array [1 .. 65536] of Char;
 
 begin
 	SetTextBuf(Input, InputBuf);
 	SetTextBuf(Output, OutputBuf);
 
-	readln(a, b);
-	if a = b then
-		writeln(-1)
-	else begin
-		culprit := 1;
-		while (culprit = a) or (culprit = b) do
-			inc(culprit);
-		writeln(culprit);
-	end;
+	readln(n);
+
+	for i := 1 to n do read(lock[i]); readln;
+
+	l := 0;
+	r := n;
+	while (r-l > 1) and (lock[l+1] = 0) do inc(l);
+	while (r-l > 1) and (lock[r] = 0) do dec(r);
+
+	writeln(r-l-1);
 end.
