252278da8459dfc206fe6f7758ba5b5acaea2494
Nenad Đapić <ndjapic@users.noreply.github.com>
2026-03-07 19:43:39 +0100

    Update C.pas

--- atcoder/abc448/C.pas
+++ atcoder/abc448/C.pas
@@ -6,12 +6,17 @@ const
 	maxn = 300 * 1000;
 	maxt = 1024 * 1024;
 var
-	n, q, i, j, mn: int32;
+	n, q, i, j: int32;
 	k, x: int8;
 	a: array [1 .. maxn] of int32;
-	b: array [0 .. 6] of int32;
+	b, a2: array [1 .. 5] of int32;
 	st: array [1 .. maxt] of int32;
 	InputBuf, OutputBuf: array [1 .. 65536] of Char;
+
+procedure combine(v: int32);
+begin
+	st[v] := min(st[2*v], st[2*v+1]);
+end;
 
 procedure build(v, l, r: int32);
 var
@@ -21,25 +26,23 @@ begin
 		m := (l+r) div 2;
 		build(2*v, l, m);
 		build(2*v+1, m+1, r);
-		st[v] := min(st[2*v], st[2*v+1]);
+		combine(v);
 	end else
 		st[v] := a[l];
 end;
 
-function query(v, vl, vr, l, r: int32): int32;
+procedure update(v, l, r, i: int32);
 var
 	m: int32;
 begin
-	if (r < vl) or (vr < l) then
-		query := high(int32)
-	else if (l <= vl) and (vr <= r) then
-		query := st[v]
-	else {if vl < vr then} begin
-		m := (vl+vr) div 2;
-		query := min(
-			query(2*v, vl, m, l, r),
-			query(2*v+1, m+1, vr, l, r)
-		);
+	if (i < l) or (r < i) then
+	else if (i <= l) and (r <= i) then begin
+		st[v] := a[i];
+	end else {if l < r then} begin
+		m := (l+r) div 2;
+		update(2*v, l, m, i);
+		update(2*v+1, m+1, r, i);
+		combine(v);
 	end;
 end;
 
@@ -51,21 +54,25 @@ begin
 
 	for i := 1 to n do read(a[i]); readln;
 	build(1, 1, n);
-	b[0] := 0;
 
 	for j := 1 to q do begin
-		read{ln}(k);
-		mn := high(int32);
+		readln(k);
 
-		for x := 1 to k+1 do begin
-			if x <= k then
-				read(b[x])
-			else
-				b[x] := n+1;
-			mn := min(mn, query(1, 1, n, b[x-1]+1, b[x]-1));
+		for x := 1 to k do begin
+			read(i);
+			b[x] := i;
+			a2[x] := a[i];
+			a[i] := high(int32);
+			update(1, 1, n, i);
 		end;
-		{readln;}
+		readln;
 
-		writeln(mn);
+		writeln(st[1]);
+
+		for x := 1 to k do begin
+			i := b[x];
+			a[i] := a2[x];
+			update(1, 1, n, i);
+		end;
 	end;
 end.
