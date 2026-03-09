9bb84cb4a82f1ee9d24789aa8cbc31e3a309f56d
Nenad Đapić <ndjapic@users.noreply.github.com>
2026-03-09 08:21:50 +0100

    Create C.pas

---
+++ atcoder/awc0020/C.pas
@@ -0,0 +1,26 @@
+program _C;
+{$MODE DELPHI}{$OPTIMIZATION LEVEL3,ON}
+uses
+	math;
+var
+	n, i, v, l, r: int32;
+	InputBuf, OutputBuf: array [1 .. 65536] of Char;
+
+begin
+	SetTextBuf(Input, InputBuf);
+	SetTextBuf(Output, OutputBuf);
+
+	readln(n);
+	read(v);
+	l := v;
+	r := v;
+
+	for i := 2 to n do begin
+		read(v);
+		l := min(l, v);
+		r := max(r, v);
+	end;
+	readln;
+
+	writeln(r-l);
+end.
