415487d0b6249696f15bacd4b39fcbdf3f9dfa3c
Nenad Đapić <ndjapic@users.noreply.github.com>
2026-03-10 11:43:30 +0100

    Create C.pas

---
+++ atcoder/adt_easy_20260310_2/C.pas
@@ -0,0 +1,31 @@
+program _C;
+{$MODE DELPHI}{$OPTIMIZATION LEVEL3,ON}
+uses
+	math;
+var
+	n, m, i, j, d: int8;
+	s, t: string;
+	c, mn: int16;
+	InputBuf, OutputBuf: array [1 .. 65536] of Char;
+
+begin
+	SetTextBuf(Input, InputBuf);
+	SetTextBuf(Output, OutputBuf);
+
+	readln(n, m);
+	readln(s);
+	readln(t);
+
+	mn := 1000;
+	for i := 0 to n-m do begin
+		c := 0;
+		for j := 1 to m do begin
+			d := ord(s[i+j]) - ord(t[j]);
+			if d < 0 then inc(d, 10);
+			inc(c, d);
+		end;
+		mn := min(mn, c);
+	end;
+
+	writeln(mn);
+end.
