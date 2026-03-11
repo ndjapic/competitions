b683fc9cb914ef6ce19e3b01bc01bbf3d0348dc8
Nenad Đapić <ndjapic@users.noreply.github.com>
2026-03-11 21:24:56 +0100

    Create B.pas

---
+++ atcoder/awc0023/B.pas
@@ -0,0 +1,25 @@
+program _B;
+{$MODE DELPHI}{$OPTIMIZATION LEVEL3,ON}
+uses
+	math;
+var
+	n, i, a, b: int32;
+	ans: int64;
+	InputBuf, OutputBuf: array [1 .. 65536] of Char;
+
+begin
+	SetTextBuf(Input, InputBuf);
+	SetTextBuf(Output, OutputBuf);
+
+	readln(n);
+
+	ans := 0;
+	for i := 1 to n-1 do begin
+		readln(a, b);
+		inc(ans, a);
+		ans := max(0, ans - b);
+	end;
+
+	readln(a);
+	writeln(ans + a);
+end.
