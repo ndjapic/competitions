e209952ab69c799b3491e8dbfba34f52080b197c
Nenad Đapić <ndjapic@users.noreply.github.com>
2026-03-11 14:20:42 +0100

    Create A.pas

---
+++ atcoder/adt_easy_20260311_1/A.pas
@@ -0,0 +1,27 @@
+program _A;
+{$MODE DELPHI}{$OPTIMIZATION LEVEL3,ON}
+var
+	n, i: int8;
+	s: string;
+	found: boolean;
+	InputBuf, OutputBuf: array [1 .. 65536] of Char;
+
+begin
+	SetTextBuf(Input, InputBuf);
+	SetTextBuf(Output, OutputBuf);
+
+	readln(n);
+	readln(s);
+
+	i := 1;
+	found := false;
+	while (i < n) and not found do begin
+		found := (s[i] = 'a') and (s[i+1] = 'b') or (s[i] = 'b') and (s[i+1] = 'a');
+		inc(i);
+	end;
+
+	if found then
+		writeln('Yes')
+	else
+		writeln('No');
+end.
