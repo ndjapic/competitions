5e7f2f68c7c711f9b28c55a05c35fcb632bca52a
Nenad Đapić <ndjapic@users.noreply.github.com>
2026-03-18 13:09:38 +0100

    Create A.pas

---
+++ atcoder/awc0028/A.pas
@@ -0,0 +1,21 @@
+program _A;
+{$MODE DELPHI}{$OPTIMIZATION LEVEL3,ON}
+var
+	n, i, k, p, claps: int32;
+	InputBuf, OutputBuf: array [1 .. 65536] of Char;
+
+begin
+	SetTextBuf(Input, InputBuf);
+	SetTextBuf(Output, OutputBuf);
+
+	readln(n, k);
+
+	claps := 0;
+	for i := 1 to n do begin
+		read(p);
+		if p >= k then inc(claps, p);
+	end;
+	readln;
+
+	writeln(claps);
+end.
