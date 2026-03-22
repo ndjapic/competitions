0a8f33f76b69492dbabb1ec48b800c7a0bd9644d
Nenad Đapić <ndjapic@users.noreply.github.com>
2026-03-22 22:21:12 +0100

    Create D.pas

---
+++ atcoder/adt_easy_20260320_1/D.pas
@@ -0,0 +1,20 @@
+program _D;
+{$MODE DELPHI}{$OPTIMIZATION LEVEL3,ON}
+var
+	n: int8;
+	x: string;
+	InputBuf, OutputBuf: array [1 .. 65536] of Char;
+
+begin
+	SetTextBuf(Input, InputBuf);
+	SetTextBuf(Output, OutputBuf);
+
+	readln(x);
+
+	n := length(x);
+	while x[n] = '0' do dec(n);
+	if x[n] = '.' then dec(n);
+
+	setlength(x, n);
+	writeln(x);
+end.
