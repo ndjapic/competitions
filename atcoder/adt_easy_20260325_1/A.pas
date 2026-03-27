27c9dc20bdb8d1f82fabf077b095f22970acf0ce
Nenad Đapić <ndjapic@users.noreply.github.com>
2026-03-27 21:45:03 +0100

    Create A.pas

---
+++ atcoder/adt_easy_20260325_1/A.pas
@@ -0,0 +1,17 @@
+program _A;
+{$MODE DELPHI}{$OPTIMIZATION LEVEL3,ON}
+var
+	a, b: int32;
+	InputBuf, OutputBuf: array [1 .. 65536] of Char;
+
+begin
+	SetTextBuf(Input, InputBuf);
+	SetTextBuf(Output, OutputBuf);
+
+	readln(a, b);
+
+	if (a div 2 = b) or (b div 2 = a) then
+		writeln('Yes')
+	else
+		writeln('No');
+end.
