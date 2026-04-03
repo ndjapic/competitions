2fe0ed0f9e9b0e5868f17a86926d63428fec72f1
Nenad Đapić <ndjapic@users.noreply.github.com>
2026-04-03 12:59:59 +0200

    Create A.pas

---
+++ atcoder/adt_easy_20260402_1/A.pas
@@ -0,0 +1,25 @@
+program _A;
+{$MODE DELPHI}{$OPTIMIZATION LEVEL3,ON}
+var
+	n, d, i, empty: int8;
+	s: string;
+	InputBuf, OutputBuf: array [1 .. 65536] of Char;
+
+begin
+	SetTextBuf(Input, InputBuf);
+	SetTextBuf(Output, OutputBuf);
+
+	readln(n, d);
+	readln(s);
+
+	empty := 0;
+	for i := 1 to n do
+		if s[i] = '.' then
+			inc(empty)
+		else if d > 0 then begin
+			inc(empty);
+			dec(d);
+		end;
+
+	writeln(empty);
+end.
