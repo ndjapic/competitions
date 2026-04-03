c7281fdc939a8549ba9dfed779bad1b5dfecf936
Nenad Đapić <ndjapic@users.noreply.github.com>
2026-04-03 13:00:44 +0200

    Create B.pas

---
+++ atcoder/adt_easy_20260402_2/B.pas
@@ -0,0 +1,22 @@
+program _B;
+{$MODE DELPHI}{$OPTIMIZATION LEVEL3,ON}
+var
+	n, i, ai: int8;
+	s: int32;
+	InputBuf, OutputBuf: array [1 .. 65536] of Char;
+
+begin
+	SetTextBuf(Input, InputBuf);
+	SetTextBuf(Output, OutputBuf);
+
+	readln(n);
+
+	s := 0;
+	for i := 1 to n-1 do begin
+		read(ai);
+		inc(s, ai);
+	end;
+	readln;
+
+	writeln(-s);
+end.
