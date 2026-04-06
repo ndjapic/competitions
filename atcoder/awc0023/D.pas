a2b6ce4073d03c29d940d8319fed0479d84e7b34
Nenad Đapić <ndjapic@users.noreply.github.com>
2026-04-06 10:23:05 +0200

    Update D.pas

--- atcoder/awc0023/D.pas
+++ atcoder/awc0023/D.pas
@@ -1,15 +1,36 @@
 program _D;
 {$MODE DELPHI}{$OPTIMIZATION LEVEL3,ON}
 uses
-	math;
+	generics.collections, generics.defaults, math;
 const
 	nn = 50;
 	ww = 100 * 100;
 var
 	n, i: int8;
-	s, t, j, p, c, w, ans: int32;
-	dpt, dpa: array [0 .. nn, 0 .. ww] of int32;
+	s, t, ans: int32;
+	p, c, w, op, ow: array [1 .. nn] of int32;
+	sp, sw: array [0 .. nn] of int32;
+	ind: tlist<int8>;
 	InputBuf, OutputBuf: array [1 .. 65536] of Char;
+
+procedure dfs(i, d: int8; x, y: int32);
+begin
+	if (x > s) or (ans <= d) or (y + sp[i] < t) then
+	else if y >= t then
+		ans := d
+	else if (i > 0) and (op[i] > 0) then begin
+		dfs(i-1, d+1, x + ow[i], y + op[i]);
+		dfs(i-1, d, x, y);
+	end;
+end;
+
+function CompareIndices(constref left, right: int8): int32;
+begin
+	result := p[left] - p[right];
+	if result = 0 then
+		result := w[left] - w[right];
+	{result := sign(p[left] - p[right] + w[right] - w[left]);}
+end;
 
 begin
 	SetTextBuf(Input, InputBuf);
@@ -17,33 +38,28 @@ begin
 
 	readln(n, s, t);
 
-	for j := 0 to s do begin
-		dpt[0, j] := 0;
-		dpa[0, j] := 0;
+	ind := tlist<int8>.create;
+	for i := 1 to n do begin
+		readln(p[i], c[i], w[i]);
+		dec(p[i], c[i]);
+		ind.add(i);
+	end;
+	ind.sort(tcomparer<int8>.construct(CompareIndices));
+
+	sw[0] := 0;
+	sp[0] := 0;
+
+	for i := 1 to n do begin
+		ow[i] := w[ind[i-1]];
+		op[i] := p[ind[i-1]];
+		sw[i] := sw[i-1] + ow[i];
+		sp[i] := sp[i-1] + max(0, op[i]);
 	end;
 
 	ans := n+1;
-	for i := 1 to n do begin
-
-		for j := 0 to s do begin
-			dpt[i, j] := dpt[i-1, j];
-			dpa[i, j] := dpa[i-1, j];
-		end;
-
-		readln(p, c, w);
-
-		for j := w to s do
-			if (dpt[i, j] < dpt[i-1, j-w] + p-c) {and (dpa[i, j] >= dpa[i-1, j-w] + 1)} then begin
-				dpt[i, j] := dpt[i-1, j-w] + p-c;
-				dpa[i, j] := dpa[i-1, j-w] + 1;
-			end;
-
-	end;
-
-	for j := 0 to s do
-		if dpt[n, j] >= t then
-			ans := min(ans, dpa[n, j]);
+	dfs(n, 0, 0, 0);
 
 	if ans > n then ans := -1;
 	writeln(ans);
+	ind.free;
 end.
