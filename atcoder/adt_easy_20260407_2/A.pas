443d85b51fc0dfc30d9cd9b3842c23646a6c9661
Nenad Đapić <ndjapic@users.noreply.github.com>
2026-04-07 15:19:52 +0200

    Create A.pas

---
+++ atcoder/adt_easy_20260407_2/A.pas
@@ -0,0 +1,23 @@
+program _A;
+{$MODE DELPHI}{$OPTIMIZATION LEVEL3,ON}
+var
+	n, i, m, a: int32;
+	InputBuf, OutputBuf: array [1 .. 65536] of Char;
+
+begin
+	SetTextBuf(Input, InputBuf);
+	SetTextBuf(Output, OutputBuf);
+
+	readln(n, m);
+
+	for i := 1 to n do begin
+		read(a);
+		dec(m, a);
+	end;
+	readln;
+
+	if m >= 0 then
+		writeln('Yes')
+	else
+		writeln('No');
+end.
