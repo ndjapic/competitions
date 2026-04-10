bc5ffc091826569c59b3e3df1609baf4676c9fe7
Nenad Đapić <ndjapic@users.noreply.github.com>
2026-04-10 11:58:51 +0200

    AtCoder Daily Training EASY 2026/04/10 18:00start
    https://atcoder.jp/contests/adt_easy_20260410_2

---
+++ atcoder/adt_easy_20260410_2/A.pas
@@ -0,0 +1,21 @@
+program _A;
+{$MODE DELPHI}{$OPTIMIZATION LEVEL3,ON}
+var
+	y: int16;
+	InputBuf, OutputBuf: array [1 .. 65536] of Char;
+
+begin
+	SetTextBuf(Input, InputBuf);
+	SetTextBuf(Output, OutputBuf);
+
+	readln(y);
+
+	if y mod 4 > 0 then
+		writeln(365)
+	else if y mod 100 > 0 then
+		writeln(366)
+	else if y mod 400 > 0 then
+		writeln(365)
+	else
+		writeln(366);
+end.
---
+++ atcoder/adt_easy_20260410_2/E.pas
@@ -0,0 +1,31 @@
+program _D;
+{$MODE DELPHI}{$OPTIMIZATION LEVEL3,ON}
+const
+	nn = 200 * 1000;
+var
+	n, a, ab: int32;
+	ans: int64;
+	dp: array [1 .. nn] of int32;
+	InputBuf, OutputBuf: array [1 .. 65536] of Char;
+
+begin
+	SetTextBuf(Input, InputBuf);
+	SetTextBuf(Output, OutputBuf);
+
+	readln(n);
+
+	for a := 1 to n do dp[a] := 0;
+
+	for a := 1 to n do begin
+		ab := a;
+		while ab <= n do begin
+			inc(dp[ab]);
+			inc(ab, a);
+		end;
+	end;
+
+	ans := 0;
+	for ab := 1 to n-1 do inc(ans, int64(dp[ab]) * dp[n - ab]);
+
+	writeln(ans);
+end.
