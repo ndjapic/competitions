6a67b937a3dd908303e2c3750087d3e7de337b0c
Nenad Đapić <ndjapic@users.noreply.github.com>
2026-04-13 14:22:48 +0200

    Update isqrt.pas

--- samples/isqrt.pas
+++ samples/isqrt.pas
@@ -4,14 +4,14 @@ uses
 
 function isqrt(a: int64): int64;
 var
-    x: int64;
+	x: int64;
 begin
-    x := min(a, high(int32));
-    while x * x > a do
-        x := (x + a div x) div 2;
-    isqrt := x;
+	x := min(a, high(int32));
+	while x * x > a do
+		x := (x + a div x) div 2;
+	isqrt := x;
 end;
 
 begin
-    writeln(isqrt(200 * 1000));
+	writeln(isqrt(200 * 1000));
 end.
