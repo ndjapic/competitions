b635065ec290bd341f50102763c39ded3d0af9df
Nenad Đapić <ndjapic@users.noreply.github.com>
2026-04-14 11:49:30 +0200

    AtCoder Daily Training EASY 2026/04/14 18:00start
    url: https://atcoder.jp/contests/adt_easy_20260414_2

---
+++ atcoder/adt_easy_20260414_2/A.pas
@@ -0,0 +1,25 @@
+program _A;
+{$MODE DELPHI}{$OPTIMIZATION LEVEL3,ON}
+uses
+	SysUtils, Classes;
+var
+	a, b: int8;
+	s: string;
+	sl: TStringList;
+	InputBuf, OutputBuf: array [1 .. 65536] of Char;
+
+begin
+	SetTextBuf(Input, InputBuf);
+	SetTextBuf(Output, OutputBuf);
+
+	sl := TStringList.Create;
+	sl.Delimiter := 'x';
+	readln(s);
+	sl.DelimitedText := s;
+
+	a := StrToInt(sl[0]);
+	b := StrToInt(sl[1]);
+
+	writeln(a * b);
+	FreeAndNil(sl);
+end.
---
+++ atcoder/adt_easy_20260414_2/D.pas
@@ -0,0 +1,30 @@
+program _D;
+{$MODE DELPHI}{$OPTIMIZATION LEVEL3,ON}
+var
+	q, i, a, volume: int32;
+	music: boolean;
+	InputBuf, OutputBuf: array [1 .. 65536] of Char;
+
+begin
+	SetTextBuf(Input, InputBuf);
+	SetTextBuf(Output, OutputBuf);
+
+	volume := 0;
+	music := false;
+
+	readln(q);
+	for i := 1 to q do begin
+		readln(a);
+
+		case a of
+			1: inc(volume);
+			2: if volume > 0 then dec(volume);
+			3: music := not music;
+		end;
+
+		if music and (volume >= 3) then
+			writeln('Yes')
+		else
+			writeln('No');
+	end;
+end.
