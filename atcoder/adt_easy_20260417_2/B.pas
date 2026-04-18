ccb227ae0db281c92df6a384a2e2c2a7ff8e7f16
Nenad Đapić <ndjapic@users.noreply.github.com>
2026-04-18 11:11:48 +0200

    AtCoder Daily Training EASY 2026/04/17 18:00start
    url: https://atcoder.jp/contests/adt_easy_20260417_2

---
+++ atcoder/adt_easy_20260417_2/B.pas
@@ -0,0 +1,21 @@
+program _B;
+{$MODE DELPHI}{$OPTIMIZATION LEVEL3,ON}
+var
+	a, b: int8;
+	ch: char;
+	s: string;
+	InputBuf, OutputBuf: array [1 .. 65536] of Char;
+
+begin
+	SetTextBuf(Input, InputBuf);
+	SetTextBuf(Output, OutputBuf);
+
+	readln(s);
+	readln(a, b);
+
+	ch := s[a];
+	s[a] := s[b];
+	s[b] := ch;
+
+	writeln(s);
+end.
