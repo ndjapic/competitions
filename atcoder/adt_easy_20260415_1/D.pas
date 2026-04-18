5e9665d23ba5549622b5c16ec478ea67c1f00075
Nenad Đapić <ndjapic@users.noreply.github.com>
2026-04-19 00:36:42 +0200

    Create D.pas

---
+++ atcoder/adt_easy_20260415_1/D.pas
@@ -0,0 +1,21 @@
+program _D;
+{$MODE DELPHI}{$OPTIMIZATION LEVEL3,ON}
+uses
+	math;
+var
+	a, b: int32;
+	r, x, y: real;
+	InputBuf, OutputBuf: array [1 .. 65536] of Char;
+
+begin
+	SetTextBuf(Input, InputBuf);
+	SetTextBuf(Output, OutputBuf);
+
+	readln(a, b);
+
+	r := sqrt(sqr(a) + sqr(b));
+	x := a / r;
+	y := b / r;
+
+	writeln(x:0:6, ' ', y:0:6);
+end.
