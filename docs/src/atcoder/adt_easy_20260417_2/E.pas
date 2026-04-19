7001fbf007fbd3e7a909ad15d94910afd2783259
Nenad Đapić <ndjapic@users.noreply.github.com>
2026-04-19 22:21:40 +0200

    Update E.pas

--- docs/src/atcoder/adt_easy_20260417_2/E.pas
+++ docs/src/atcoder/adt_easy_20260417_2/E.pas
@@ -3,62 +3,27 @@ program _E;
 const
 	nn = 200 * 1000;
 var
-	n, m, i, u, v, c, c2: int32;
-	found: boolean;
-	adj, deg: array [1 .. nn] of int32;
-	sib, tar: array [-nn .. nn] of int32;
-	seen: array [1 .. nn] of boolean;
+	n, i, k, x: int32;
+	a: array [1 .. nn] of int32;
 	InputBuf, OutputBuf: array [1 .. 65536] of Char;
-
-procedure addarrow(u, v, i: int32);
-begin
-	sib[i] := adj[u];
-	adj[u] := i;
-	tar[i] := v;
-	inc(deg[v]);
-	if deg[v] = 2 then inc(c2);
-end;
 
 begin
 	SetTextBuf(Input, InputBuf);
 	SetTextBuf(Output, OutputBuf);
 
-	readln(n, m);
+	readln(n);
 
-	for v := 1 to n do begin
-		adj[v] := 0;
-		seen[v] := false;
-		deg[v] := 0;
-	end;
-
-	c2 := 0;
-	for i := 1 to m do begin
-		readln(u, v);
-		addarrow(u, v, i);
-		addarrow(v, u, -i);
-	end;
-
-	c := 0;
-	if n = m then begin
-		u := 1;
-		while not seen[u] do begin
-			seen[u] := true;
-			inc(c);
-			i := adj[u];
-			found := false;
-
-			while (i <> 0) and not found do begin
-				v := tar[i];
-				found := not seen[v];
-				i := sib[i];
-			end;
-
-			if found then u := v;
+	k := 0;
+	for i := 1 to n do begin
+		read(x);
+		if (k >= 3) and (a[k-2] = x) and (a[k-1] = x) and (a[k] = x) then
+			dec(k, 3)
+		else begin
+			inc(k);
+			a[k] := x;
 		end;
 	end;
+	readln;
 
-	if (c = n) and (c2 = n) and (v = 1) then
-		writeln('Yes')
-	else
-		writeln('No');
+	writeln(k);
 end.
