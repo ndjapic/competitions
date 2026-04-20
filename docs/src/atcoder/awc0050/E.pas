bbecfaa7ea1944b04458de169fece5cd0efa1c95
Nenad Đapić <ndjapic@users.noreply.github.com>
2026-04-20 20:20:08 +0200

    Update E.pas

--- docs/src/atcoder/awc0050/E.pas
+++ docs/src/atcoder/awc0050/E.pas
@@ -1,43 +1,87 @@
 program _E;
 {$MODE DELPHI}{$OPTIMIZATION LEVEL3,ON}
-uses
-	generics.collections,
-	generics.defaults;
 const
 	nn = 50;
 	mm = 18;
+	vv = 1 shl mm;
 	inf = int64(1) shl 60;
+type
+	TPrioQueue<_T> = class
+	public
+		items: array [1 .. vv] of _T;
+		inv: array [1 .. vv] of int32;
+		n: int32;
+		constructor Create();
+		function prior(l, r: _T): boolean;
+		procedure setItem(v: int32; x: _T);
+		procedure swim(v: int32; x: _T);
+		procedure enqueue(x: _T);
+		procedure sink(u: int32; x: _T);
+		procedure dequeue(u: int32);
+	end;
+
 var
 	n, m, i, j: int8;
-	mask: int32;
-	eij: int8;
-	ans: int64;
+	u, v, eij: int32;
 	c, e: array [0 .. nn] of int32;
-	adj: array [0 .. mm] of tlist<int8>;
-	link: array [0 .. 1 shl mm, 0 .. mm] of int8;
+	dist: array [0 .. vv] of int64;
+	pq: TPrioQueue<int32>;
 	InputBuf, OutputBuf: array [1 .. 65536] of Char;
 
-procedure dfs(j: int8; mask: int32; total: int64);
-var
-	i: int8;
+constructor TPrioQueue<_T>.Create();
 begin
-	if total < ans then begin
-		{j := link[mask, j];}
-		if j >= m then
-			ans := total
-		else begin
-			if odd(mask shr j) then
-				dfs(j+1, mask, total)
-			else
-				for i in adj[j] do
-					dfs(j+1, mask or e[i], total + c[i]);
-		end;
-	end;
+	n := 0;
 end;
 
-function compare(constref left, right: int8): int32;
+function TPrioQueue<_T>.prior(l, r: _T): boolean;
 begin
-	result := + c[left] - c[right];
+	prior := dist[l] < dist[r];
+end;
+
+procedure TPrioQueue<_T>.setItem(v: int32; x: _T);
+begin
+	items[v] := x;
+	inv[x] := v;
+end;
+
+procedure TPrioQueue<_T>.swim(v: int32; x: _T);
+var
+	u: int32;
+begin
+	u := v div 2;
+	while (v > 1) and prior(x, items[u]) do begin
+		setItem(v, items[u]);
+		v := u;
+		u := v div 2;
+	end;
+	setItem(v, x);
+end;
+
+procedure TPrioQueue<_T>.enqueue(x: _T);
+begin
+	inc(n);
+	swim(n, x);
+end;
+
+procedure TPrioQueue<_T>.sink(u: int32; x: _T);
+var
+	v: int32;
+begin
+	v := u * 2;
+	if (v+1 <= n) and prior(items[v+1], items[v]) then inc(v);
+	while (v <= n) and prior(items[v], x) do begin
+		setItem(u, items[v]);
+		u := v;
+		v := u * 2;
+		if (v+1 <= n) and prior(items[v+1], items[v]) then inc(v);
+	end;
+	setItem(u, x);
+end;
+
+procedure TPrioQueue<_T>.dequeue(u: int32);
+begin
+	dec(n);
+	sink(u, items[n+1]);
 end;
 
 begin
@@ -49,35 +93,38 @@ begin
 	for i := 0 to n-1 do read(c[i]);
 	readln;
 
-	for j := 0 to m-1 do adj[j] := tlist<int8>.create;
-
 	for i := 0 to n-1 do begin
 		e[i] := 0;
 		for j := 0 to m-1 do begin
 			read(eij);
-			if eij = 1 then begin
-				{if e[i] = 0 then} adj[j].add(i);
-				inc(e[i], 1 shl j);
-			end;
+			inc(e[i], eij shl j);
 		end;
 		readln;
 	end;
 
-	for j := 0 to m-1 do
-		adj[j].sort(tcomparer<int8>.construct(compare));
+	for v := 0 to (1 shl m) - 1 do dist[v] := inf;
+	dist[0] := 0;
+	pq := TPrioQueue<int32>.Create();
+	for v := 0 to (1 shl m) - 1 do pq.enqueue(v);
 
-	for mask := 0 to (1 shl m) - 1 do
-		for j := m downto 0 do begin
-			link[mask, j] := j;
-			if odd(mask shr j) then
-				link[mask, j] := link[mask, j+1];
+	while pq.n > 0 do begin
+		u := pq.items[1];
+		pq.dequeue(1);
+
+		for i := 0 to n-1 do begin
+			v := u or e[i];
+			if dist[v] > dist[u] + c[i] then begin
+				dist[v] := dist[u] + c[i];
+				pq.swim(pq.inv[v], v);
+			end;
 		end;
+	end;
 
-	ans := inf;
-	dfs(0, 0, 0);
+	v := (1 shl m) - 1;
+	if dist[v] < inf then
+		writeln(dist[v])
+	else
+		writeln(-1);
 
-	if ans = inf then ans := -1;
-	writeln(ans);
-
-	for j := 0 to m-1 do adj[j].free;
+	pq.Free;
 end.
