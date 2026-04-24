4392ae8c58297fdad34561beb153e7e7d231701c
Nenad Đapić <ndjapic@users.noreply.github.com>
2026-04-24 22:01:21 +0200

    Create C.pas

---
+++ docs/src/atcoder/awc0055/C.pas
@@ -0,0 +1,54 @@
+program _C;
+{$MODE DELPHI}{$OPTIMIZATION LEVEL3,ON}
+uses
+	generics.collections,
+	generics.defaults;
+var
+	n, i, j, elm, ans: int32;
+	w, c: tlist<int32>;
+	InputBuf, OutputBuf: array [1 .. 65536] of Char;
+
+begin
+	SetTextBuf(Input, InputBuf);
+	SetTextBuf(Output, OutputBuf);
+	randomize;
+
+	readln(n);
+	w := tlist<int32>.create;
+	c := tlist<int32>.create;
+	try
+
+		for i := 0 to n-1 do begin
+			read(elm);
+			w.add(elm);
+			w.exchange(i, random(i+1));
+		end;
+		readln;
+		w.sort;
+
+		for j := 0 to n-1 do begin
+			read(elm);
+			c.add(elm);
+			c.exchange(j, random(j+1));
+		end;
+		readln;
+		c.sort;
+
+		i := 0;
+		j := 0;
+		ans := 0;
+		while (i < n) and (j < n) do
+			if w[i] <= c[j] then begin
+				inc(ans);
+				inc(i);
+				inc(j);
+			end else
+				inc(j);
+
+		writeln(ans);
+
+	finally
+		w.free;
+		c.free;
+	end;
+end.
