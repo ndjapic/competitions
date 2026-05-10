6a68703c540c61d26e440c51649c3df594fac449
Nenad Đapić <ndjapic@users.noreply.github.com>
2026-05-10 23:39:13 +0200

    Update _AVL_set_simple_minmax.pas

--- docs/src/samples/_AVL_set_simple_minmax.pas
+++ docs/src/samples/_AVL_set_simple_minmax.pas
@@ -1,16 +1,16 @@
 program AtCoderMultiSet;
-{$mode delphi}{$inline on}
+{$MODE DELPHI}{$OPTIMIZATION LEVEL3,ON}
 uses
 	Math;
 const
-	MAX_NODES = 400005; // Прилагодите максималном броју упита (N)
+	MAX_NODES = 200 * 1000;
 type
 	TNode = record
 		Key: Int32;
 		Count, Height, Left, Right: Int32;
 	end;
 var
-	Tree: array[0..MAX_NODES] of TNode;
+	Tree: array[0 .. MAX_NODES] of TNode;
 	Root: Int32 = 0;
 	NodeCount: Int32 = 0;
 
@@ -47,8 +47,7 @@ begin
 end;
 
 function RightRotate(Y: Int32): Int32;
-var
-	X, T2: Int32;
+var X, T2: Int32;
 begin
 	X := Tree[Y].Left;
 	T2 := Tree[X].Right;
@@ -60,8 +59,7 @@ begin
 end;
 
 function LeftRotate(X: Int32): Int32;
-var
-	Y, T2: Int32;
+var Y, T2: Int32;
 begin
 	Y := Tree[X].Right;
 	T2 := Tree[Y].Left;
@@ -73,12 +71,10 @@ begin
 end;
 
 function BalanceNode(N: Int32): Int32;
-var
-	Balance: Int32;
+var Balance: Int32;
 begin
 	UpdateHeight(N);
 	Balance := GetBalance(N);
-
 	if Balance > 1 then begin
 		if GetBalance(Tree[N].Left) < 0 then
 			Tree[N].Left := LeftRotate(Tree[N].Left);
@@ -87,8 +83,7 @@ begin
 		if GetBalance(Tree[N].Right) > 0 then
 			Tree[N].Right := RightRotate(Tree[N].Right);
 		Result := LeftRotate(N);
-	end else
-		Result := N;
+	end else Result := N;
 end;
 
 function Insert(N: Int32; K: Int32): Int32;
@@ -96,12 +91,12 @@ begin
 	if N = 0 then
 		Result := NewNode(K)
 	else if K = Tree[N].Key then begin
-		Inc(Tree[N].Count); // Елемент већ постоји, само увећавамо бројач
+		Inc(Tree[N].Count);
 		Result := N;
 	end else begin
 		if K < Tree[N].Key then
 			Tree[N].Left := Insert(Tree[N].Left, K)
-		else {if K > Tree[N].Key then}
+		else
 			Tree[N].Right := Insert(Tree[N].Right, K);
 		Result := BalanceNode(N);
 	end;
@@ -109,50 +104,44 @@ end;
 
 function FindMinNode(N: Int32): Int32; inline;
 begin
-	while Tree[N].Left <> 0 do N := Tree[N].Left;
 	Result := N;
+	if N <> 0 then
+		while Tree[Result].Left <> 0 do
+			Result := Tree[Result].Left;
 end;
 
 function FindMaxNode(N: Int32): Int32; inline;
 begin
-	while Tree[N].Right <> 0 do N := Tree[N].Right;
 	Result := N;
+	if N > 0 then
+		while Tree[Result].Right <> 0 do
+			Result := Tree[Result].Right;
 end;
 
 function Delete(N: Int32; K: Int32; C: Int32): Int32;
-var
-	Temp: Int32;
+var Temp: Int32;
 begin
+	Result := N;
 	if N > 0 then begin
 		if K < Tree[N].Key then
 			Tree[N].Left := Delete(Tree[N].Left, K, C)
-		else if K > Tree[N].Key then begin
-			Tree[N].Right := Delete(Tree[N].Left, K, C); // Исправка за уклањање десно
-			// На први поглед делује да је претходни ред сувишан
-			Tree[N].Right := Delete(Tree[N].Right, K, C); // Овде и даље нешто није исправно
-		end else if Tree[N].Count > C then begin
-			// Пронашли смо елемент
-			Dec(Tree[N].Count, C); // Смањујемо број копија без брисања чвора
-			Result := N;
-		end else if Tree[N].Right = 0 then
-			// Стварно брисање чвора јер је број копија пао на 0
-			// Замена тренутног чвора његовим јединим дететом (или нулом)
-			N := Tree[N].Left
+		else if K > Tree[N].Key then
+			Tree[N].Right := Delete(Tree[N].Right, K, C)
+		else if Tree[N].Count > C then
+			Dec(Tree[N].Count, C)
+		else if Tree[N].Right = 0 then
+			Result := Tree[N].Left
 		else if Tree[N].Left = 0 then
-			N := Tree[N].Right
+			Result := Tree[N].Right
 		else begin
 			Temp := FindMinNode(Tree[N].Right);
 			Tree[N].Key := Tree[Temp].Key;
 			Tree[N].Count := Tree[Temp].Count;
-			// Бришемо заменски чвор са десне стране (сада прослеђујемо целу његову фреквенцију)
+			// Бришемо заменски чвор из десног подстабла
 			Tree[N].Right := Delete(Tree[N].Right, Tree[Temp].Key, Tree[Temp].Count);
 		end;
+		if Result > 0 then Result := BalanceNode(Result);
 	end;
-
-	if N = 0 then
-		Result := 0
-	else
-		Result := BalanceNode(N);
 end;
 
 var
@@ -163,11 +152,8 @@ var
 begin
 	SetTextBuf(Input, InputBuf);
 	SetTextBuf(Output, OutputBuf);
-	// Брзи улаз/излаз за такмичарско програмирање
-	Reset(Input); Rewrite(Output);
 
 	if not SeekEof then Read(q);
-
 	for i := 1 to q do begin
 		Read(tp);
 		case tp of
@@ -185,7 +171,10 @@ begin
 			3: begin
 				MinN := FindMinNode(Root);
 				MaxN := FindMaxNode(Root);
-				Writeln(Tree[MaxN].Key - Tree[MinN].Key);
+				if Root <> 0 then
+					Writeln(Tree[MaxN].Key - Tree[MinN].Key)
+				else
+					Writeln(0);
 			end;
 
 		end;
