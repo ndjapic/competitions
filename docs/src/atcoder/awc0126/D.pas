7dbc2e175746eac220adba62df4e962dc44a0f59
Nenad Đapić <ndjapic@users.noreply.github.com>
2026-08-03 19:23:28 +0200

    Update D.pas

--- docs/src/atcoder/awc0126/D.pas
+++ docs/src/atcoder/awc0126/D.pas
@@ -1,25 +1,20 @@
 program _D;
 {$MODE DELPHI}{$OPTIMIZATION LEVEL3,ON}
-// #MLE #TLE #pathfinder01 #bfs #queue #set
+// #pathfinder01 #bfs #queue #set
 uses
 	Generics.Collections;
 const
 	NN = 25;
 	DD = 7;
 	POW2 = 1 shl NN;
-type
-	TPath = record
-		v, d: int32;
-	end;
 var
 	n, j, k, d: int8;
 	u, v, p0, p2: int32;
 	s, t: string;
-	p: TPath;
-	adj: array [0 .. POW2] of TDictionary<int32, boolean>;
+	adj: TDictionary<int32, boolean>;
 	dist: array [0 .. POW2] of int32;
 	divs: array [1 .. DD] of int8;
-	bfs: TQueue<TPath>;
+	bfs: TQueue<int32>;
 	InputBuf, OutputBuf: array [1 .. 65536] of Char;
 
 function s2v(s: string): int32;
@@ -46,54 +41,45 @@ begin
 			divs[k] := d;
 		end;
 
-	for u := 0 to p0 - 1 do begin
-		adj[u] := TDictionary<int32, boolean>.Create;
-		dist[u] := p0;
-	end;
+	for u := 0 to p0 - 1 do dist[u] := p0;
 
-	for u := 0 to p0 - 1 do begin
+	readln(s);
+	v := s2v(s);
+	dist[v] := 0;
+	bfs := TQueue<int32>.Create;
+	adj := TDictionary<int32, boolean>.Create;
+	bfs.Enqueue(v);
+
+	while bfs.Count > 0 do begin
+		u := bfs.Dequeue;
+		adj.Clear;
 
 		for j := 1 to k do begin
 			p2 := 1 shl divs[j];
 			v := (p0 - 1) div (p2 - 1) * (u mod p2);
-			if v <> u then
-				adj[u].AddOrSetValue(v, true);
+			adj.AddOrSetValue(v, true);
 		end;
 
 		v := 2 * u;
 		if v >= p0 then dec(v, p0 - 1);
-		if v <> u then begin
-			adj[u].AddOrSetValue(v, true);
-			adj[v].AddOrSetValue(u, true);
-		end;
+		adj.AddOrSetValue(v, true);
 
-	end;
+		v := (u mod 2 * p0 + u) div 2;
+		adj.AddOrSetValue(v, true);
 
-	readln(s);
-	p.v := s2v(s);
-	p.d := 0;
-	dist[p.v] := 0;
-	bfs := TQueue<TPath>.Create;
-	bfs.Enqueue(p);
-
-	while bfs.Count > 0 do begin
-		p := bfs.Dequeue;
-		u := p.v;
-		if dist[u] = p.d then begin
-			inc(p.d);
-			for v in adj[u].Keys do
-				if dist[v] > p.d then begin
-					dist[v] := p.d;
-					p.v := v;
-					bfs.Enqueue(p);
-				end;
-		end;
+		d := dist[u] + 1;
+		for v in adj.Keys do
+			if dist[v] > d then begin
+				dist[v] := d;
+				bfs.Enqueue(v);
+			end;
 	end;
 
 	readln(t);
 	v := s2v(t);
 	if dist[v] = p0 then dist[v] := -1;
 	writeln(dist[v]);
-	for u := 0 to p0 - 1 do adj[u].Free;
+
+	adj.Free;
 	bfs.Free;
 end.
